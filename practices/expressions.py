a6d83c23340df3f0a9cb2ab3f19c7022c0399eef
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-09-04 13:43:32 -0600

    expressions

---
+++ practices/expressions.py
@@ -0,0 +1,40 @@
+# AS 6th expressions notes. 
+
+# what is an algorithm?
+    # a set of instructions 
+# list steps in an algorithm
+    # a collection of numbers and symbols.
+#ex
+name = input("what is your name?")
+print("hello" , name)
+
+#algorithm for area
+length = 5
+width = 20
+area = length * width 
+print(length, "*" , width, "=", area)
+#steps
+#1 pieces of info I need
+#2 process info
+#3 provide output
+
+person1 = 14
+person2 = 14
+person3 = 14
+person4 = 14
+
+average = (person1 + person2 + person3 + person4)/4
+
+print("the average age is" , average)
+
+#math equations
+num_1 = 12
+num_2 = 4
+
+print("addition:" , num_1 + num_2)
+print("subtraction:" , num_1 - num_2)
+print("multiplication:" , num_1 * num_2)
+print("dvision:" , num_1 / num_2)
+print("exponents:" , num_1 ** num_2)
+print("integer division:" , num_1 // num_2)
+print("mudulo (remainder):" , num_1 % num_2)
