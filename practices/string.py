d9b6decab2bfb42aa47b5485294951b5105be12b
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-09-08 13:44:47 -0600

    wassup

--- practices/string.py
+++ practices/string.py
@@ -1,3 +1,32 @@
-# AS 6th string.py
+# AS 6th strings notes
 
-print("I did it!")
+print("I did it!") 
+#What makes something a string?
+    #a string is just a collection of symbols held together by quotation marks.
+    # ex: "hi brotato your cool" or 'hi brotato your cool'
+
+#examples of strings
+name = input("what is your name:").strip().lower()
+
+sentence = "the quick brown fox jumps over the lazy dog."
+
+first_name = input("what is your first name:\n").strip().title()
+
+last_name = input
+
+print ("welcome to my program", name)
+#Why do we have strings?
+
+#How do stupid proofing and sanitization relate to each other?
+#users = stupid. 
+
+#What is debugging?
+
+#How do you debug the different types of errors?
+
+#Describe what each of the methods below does:
+#find() 
+#concatenate (add)
+#upper() makes all upper
+#lower() makes all lower
+#strip() strip makes the user less dumb
