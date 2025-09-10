421af2972a646be2c186810ea15b5e25576ca870
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-09-10 13:39:36 -0600

    hi

---
+++ practices/sillysentences.py
@@ -0,0 +1,9 @@
+#AS 6th silly sentences - python
+
+adjective = input("please pick an adjective!!")
+color = input("now, pick a color!!")
+noun = input("then, pick a noun!!")
+animal = input("choose an animal!!")
+
+print("you are walking down a dark path while", adjective, 
+"you look over your shoulder and see a", color, animal, )
