b873266075c35695ff36657c18494bd2900f061c
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-09-11 01:46:28 +0000

    silly

--- practices/sillysentences.py
+++ practices/sillysentences.py
@@ -4,6 +4,9 @@ adjective = input("please pick an adjective!!")
 color = input("now, pick a color!!")
 noun = input("then, pick a noun!!")
 animal = input("choose an animal!!")
+verb = input("choose a verb!!")
+exclamation = input("write a common thing people say when surprised!!")
 
 print("you are walking down a dark path while", adjective, 
-"you look over your shoulder and see a", color, animal, )
+"you look over your shoulder and see a", color, animal, "the animal has a", noun, "in its mouth it starts to run at you so you" 
+, verb, "as fast as you can", "you wake up and say", exclamation, "you realize it was all just a dream" + "!") 
