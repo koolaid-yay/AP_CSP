d1fecfcbd584718941bedf0952c0e31b9e6a7cf3
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-09-17 13:43:46 -0600

    helloooooo

---
+++ notes/conditional.py
@@ -0,0 +1,47 @@
+# AS 6th conditionals note
+
+num = int(input("tell me a whole number: "))
+
+if num < 10:
+    print(f"{num} is a single digit number")
+elif num < 100:
+    print(f"{num} is not single digit number")
+elif num == 4:
+    print("that is the winning number!")
+else:
+    print(f"{num} is a big number")
+
+name = input("please tell me your name")
+
+if name == "silly man":
+    print("HII you are so awesome!")
+elif name == "extra silly man":
+    print("you are the best awesome man!!")
+else:
+    print(f"hello {name} you are not so awesome!")
+
+# if is a true or false statement
+# if not correct it is boolean statement or condition
+
+# What puts something inside of the “if” statement?
+    # 
+# What do if statements do?
+    #tell the computer if its correct it will run
+# What are boolean statements? 
+    # the same as conditonal
+# What do else statements do?
+    #clarify that the if statement is wrong
+# What kind of statement do you use if you have more than 2 needed outcomes?
+    #
+# What do each of the different symbols mean in conditionals?
+    #
+# < less than
+# > greater than
+# <= less than or equal to
+# >= greater or equal to
+# == equal
+# != not equal
+# What are the 3 logical operators?
+# What are logical operators for?
+# What does a nested conditional statement do?
+
