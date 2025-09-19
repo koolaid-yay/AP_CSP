fc85e667070d16d7af19ba610f31d7fd72a01dd6
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-09-19 13:44:46 -0600

    pls work pls

--- practices/financecalc.py
+++ practices/financecalc.py
@@ -1,3 +1,9 @@
 #AS 6th financial calculator
 
-input
+income = float(input("please put monthly income: "))
+rent = float(input("tell me your rent please: "))
+utilities = float(input("show me your utilities: "))
+groceries = float(input("show me your groceries: "))
+funds = income - rent - utilities - groceries 
+
+print 
