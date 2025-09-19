0e4be9f899fa1932a7b02aa08b51ffd0f1c05770
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-09-19 13:29:56 -0600

    HIIIIIIIIIIIIIIIIIIII

--- practices/timeday.py
+++ practices/timeday.py
@@ -1,2 +1,16 @@
 # AS 6th time of day
 
+import datetime 
+
+current = datetime.datetime.now()
+hour = current.hour
+
+print(f"the time is: {current}")
+print(f"hour is: {hour}")
+
+if hour < 12:
+    print("good morning!!!!!!!")
+elif hour < 18:
+    print("good afternoon!!!!!!!!!!!!!!!!!!!!!!")
+else:
+    print("good evening !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
