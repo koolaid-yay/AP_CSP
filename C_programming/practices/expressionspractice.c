6482b46ead8c57702c281da93c1a22694b28c530
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-10-08 13:43:58 -0600

    AHHHH

--- C_programming/practices/expressionspractice.c
+++ C_programming/practices/expressionspractice.c
@@ -3,8 +3,25 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void){
-    
+int main(){
+    int eq1 = 7-24/8*4+6;
+    printf("this equation equals %d\n", eq1);
 
+    int eq2 = 18/3-7+2*5;
+    printf("this equation equals %d\n", eq2);
 
+    int eq3 = 6*4/12+72/8-9;
+    printf("this equation equals %d\n", eq3);
+
+    int eq4 = (17-6/2)+4*3;
+    printf("this equation equals %d\n", eq4);
+
+    int eq5 = -2*(1*4-2/2)+(6+2-3);
+    printf("this equation equals %d\n", eq5);
+
+    int eq6 = -1*((3-4*7)/5)-2*24/6;
+    printf("this equation equals %d\n", eq6);
+
+    int eq7 = (3* pow(5, 2)/15)* pow(2, 2)+ 
+    printf("this equation equals %d\n", eq6); 
 }
