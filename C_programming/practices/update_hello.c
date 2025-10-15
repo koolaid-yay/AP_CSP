ce2e8ce176e8d9c2c2940644b51066d1ea983d03
koolaid-yay <adelheid.shomaker@ucas-edu.net>
2025-10-15 13:42:59 -0600

    i am losing it btw

--- C_programming/practices/update_hello.c
+++ C_programming/practices/update_hello.c
@@ -1,3 +1,17 @@
 // AS 6th update hello world - C
 #include <stdio.h>
 
+int main (char* name){
+    printf("hello %s\n", name);
+
+    char* get_name(void);
+    char *name;
+    printf("what is your name: ");
+    scanf("%s",name);
+    return name;
+
+
+
+
+    return 0; 
+}
